c7b1e2f251245e7c028c94377249f84071a51058
anavollu <anavollu@id.uff.br>
2023-04-25 11:29:04 -0300

    feat: add whatsapp message on link

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -112,7 +112,7 @@ export const load = (async () => {
 				socialMedia: [
 					{
 						title: 'whatsapp',
-						link: 'https://wa.me/5521982800111',
+						link: 'https://api.whatsapp.com/send?phone=5521982800111&text=Ol%C3%A1!%20Gostaria%20de%20agendar%20um%20hor%C3%A1rio%20para%20uma%20sess%C3%A3o%20de%20maquiagem%20e/ou%20penteado%20com%20voc%C3%AA.%20Voc%C3%AA%20poderia%20me%20informar%20quais%20s%C3%A3o%20seus%20hor%C3%A1rios%20dispon%C3%ADveis?',
 						icon: 'https://raw.githubusercontent.com/anavollu/site-cynthia/c4cc42f8c9653b63a3ea0d54da359a1d75cfce6e/src/lib/assets/whatsapp-icon.svg',
 						contact: '(21) 98280-0111'
 					},
@@ -132,7 +132,7 @@ export const load = (async () => {
 				image:
 					'https://raw.githubusercontent.com/anavollu/site-cynthia/main/figma-images/contato-img.jpg',
 				button: 'Agendar horário',
-				link: 'https://wa.me/5521982800111'
+				link: 'https://api.whatsapp.com/send?phone=5521982800111&text=Ol%C3%A1!%20Gostaria%20de%20agendar%20um%20hor%C3%A1rio%20para%20uma%20sess%C3%A3o%20de%20maquiagem%20e/ou%20penteado%20com%20voc%C3%AA.%20Voc%C3%AA%20poderia%20me%20informar%20quais%20s%C3%A3o%20seus%20hor%C3%A1rios%20dispon%C3%ADveis?'
 			},
 			footer: {
 				description: 'Desenvolvido por Ana Carolina Vollu',
